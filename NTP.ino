199ac49bb27c5d71efb73484d8e5d919d89c3572
Mohamed Nehad <mohammednehad450@gmail.com>
2023-08-19 13:27:46 +0300

    chore: Move NTPConnect to seprate file

---
+++ NTP.ino
@@ -0,0 +1,21 @@
+#include <time.h>
+#define TIME_ZONE -2
+
+time_t now;
+time_t nowish = 1510592825;
+
+void NTPConnect(void) {
+  Serial.print("Setting time using SNTP");
+  configTime(TIME_ZONE * 3600, 0 * 3600, "pool.ntp.org", "time.nist.gov");
+  now = time(nullptr);
+  while (now < nowish) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("done!");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+}
